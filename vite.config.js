cb46dab71404a3c549f304bacacc8ad3b5fe6daa
Dereje Masresha <dbazmm3@gmail.com>
2025-03-09 23:44:35 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Amazon-Clone/",
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
-            if (id.includes("firebase")) return "firebase-vendor";
-            return "vendor"; // Other dependencies
-          }
-        },
-      },
-    },
-  },
+  base: "/",
 });
